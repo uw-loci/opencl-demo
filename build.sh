e75b0ecd8a9b7e8724538425d337d8026be164f9
Elijah Mirecki <elimirks@gmail.com>
2016-01-19 15:20:21 -0500

    Updated the build scrip to compile for JRE 6 (the standard ImageJ Java runtime)

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-javac -cp javacl-1.0-beta-4-shaded.jar:ij.jar:src src/plugin/OpenCL_Demo.java
-jar cf javacldemo_.jar bitrev1.cl bitrev2.cl dfht3.cl profile1.cl rad4but1.cl rad4but2.cl rowtemp1.cl rowtemp2.cl sobel.cl plugins.config -C src .
+JRE_VERSION=1.6
+CLASSPATH=javacl-1.0-beta-4-shaded.jar:ij.jar:src
+COMPILE_SOURCES="src/plugin/OpenCL_Demo.java src/plugin/Iterative_Deconvolve_3D.java"
+JAR_NAME=javacldemo_.jar
+EXTRA_JAR_INCLUDE="bitrev1.cl bitrev2.cl dfht3.cl profile1.cl rad4but1.cl rad4but2.cl rowtemp1.cl rowtemp2.cl sobel.cl plugins.config"
+javac -source $JRE_VERSION -target $JRE_VERSION -cp $CLASSPATH $COMPILE_SOURCES
+jar cf $JAR_NAME $EXTRA_JAR_INCLUDE -C src .
